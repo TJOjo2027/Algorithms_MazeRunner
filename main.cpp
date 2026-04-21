17bc17bb9ecdc8991536533a2dbce2fc31c4e8cf
Aubrey Eman Bedford <Blaze@Aubreys-MacBook-Pro.local>
2026-04-21 10:52:33 -0500

    Added SDL2 dependency in CMake for MacOs

--- main.cpp
+++ main.cpp
@@ -4,11 +4,20 @@
  */
 
 #include <iostream>
+#include <SDL.h>
 
 using namespace std;
 
 int main() {
-    cout << "Lets cook this maze!" << endl;
 
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+
+
+    cout << "Lets cook this maze!" << endl;
+    SDL_Quit();
     return 0;
 }
