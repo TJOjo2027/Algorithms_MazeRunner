01e666258a6718022ca90b82f75aaee0f5f3ed4f
Meka64 <powerntma@gmail.com>
2026-04-27 21:56:29 -0500

    DFS Helpers
    - Added iterationCurrent
    - Added iterationAdvance
    - Added iterationretreat
    - Added iterationDone
    - Added Direction currDir
    - Added set<point> visited
    - Added stack<DIRECTION> dfsPath

--- RaceCarDriver.h
+++ RaceCarDriver.h
@@ -16,9 +16,54 @@ class RaceCarDriver{
 private:
 	Racer* car;
 
+	// Stack used to track taken by car
+	stack<DIRECTION> dfsPath;
+
+	// Using a set to track visited locations
+	set<point> visited;
+
+	// Current direction of the car
+	DIRECTION currDir;
+
+	point iterationCurrent(point currP) {
+		switch(currDir){
+			case EAST:  currP.x++; break;
+			case SOUTH: currP.y++; break;
+			case WEST:  currP.x--; break;
+			case NORTH: currP.y--; break;
+		}
+		return currP;
+	}
+
+	void iterationAdvance(){
+		switch(currDir){
+			case EAST:  currDir = SOUTH; break;
+			case SOUTH: currDir = WEST; break;
+			case WEST:  currDir = NORTH; break;
+			case NORTH: currDir = EAST; break;
+		}
+	}
+
+	void iterationRetreat(){
+		switch(currDir){
+			case EAST:  currDir = NORTH; break;
+			case SOUTH: currDir = EAST; break;
+			case WEST:  currDir = SOUTH; break;
+			case NORTH: currDir = WEST; break;
+		}
+	}
+
+	bool iterationDone(DIRECTION trialDir, point location) const {
+		return trialDir >= 4;
+	}
+
+
 public:
 	RaceCarDriver(Racer* p = nullptr): car{p}{}
 
+
+
+	// Booth's Random Next Move Implementation
 	DIRECTION nextMove(){
 		vector<DIRECTION> pool;
 
@@ -30,15 +75,28 @@ public:
 		return pool[rand() % pool.size()];
 	}
 
-	DIRECTION DFSNextMove(){
+	// Emeka's DFS Next Move Implementation
+	DIRECTION DFSNextMove() {
+
+		// Retrive the current location of the car
+		point currLoc = car->getLocation();
+
+		// Mark the current location as visited
+		visited.insert(currLoc);
+
+		DIRECTION nextDir;
+		for(int i = 0; i < 4; i++){
+
 		
-	}
-
-	DIRECTION BFSNextMove(){
 
 	}
+	
 
-	DIRECTION FloodFillNextMove(){
+	DIRECTION BFSNextMove() {
+
+	}
+
+	DIRECTION FloodFillNextMove() {
 		
 	}
 
